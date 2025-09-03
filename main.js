85783ef7db1233ca1f7dc9c355951a28e66f6553
Yoshicaballo <jaydrien.cruz-rivera@innovationcharter.org>
2025-09-03 16:26:19 +0000

    Added output "I am Quagmire"

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
-let ti = new TextInterface(app, "Example Text Interface Program");
+let ti = new TextInterface(app, "Robert do look");
 
 ti.output("Welcome to my program!");
 let name = await ti.prompt("What is your name?");
+
 ti.output("Hello, " + name);
-
-
+ti.output("I am Quagmire");
