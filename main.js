43f171e4cac5cbd2960b08adae2568970cedfa06
Yoshicaballo <jaydrien.cruz-rivera@innovationcharter.org>
2025-09-04 18:40:58 +0000

    clear function UNFINISHED!!!!!!!!!!!

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
-let ti = new TextInterface(app, "Robert do look");
+let ti = new TextInterface(app, "Do look");
 
 ti.output("Welcome to my program!");
 let name = await ti.prompt("What is your name?");
+// remove previous outputs
+ti.clear(); 
 
 ti.output("Hello, " + name);
 ti.output("I am Quagmire");
+
+
