325d178274daf9742a3e8f2aaa255d44e8b25866
Yoshicaballo <jaydrien.cruz-rivera@innovationcharter.org>
2025-09-05 18:35:29 +0000

    Quahog is getting added soon

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import "./style.css";
 import { TextInterface } from "text-interface";
 
+let introRepeating = true;
+if (introRepeating){
+// loop this intro until saying yes
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
 let ti = new TextInterface(app, "Do look");
@@ -8,9 +11,14 @@ let ti = new TextInterface(app, "Do look");
 ti.output("Welcome to my program!");
 let name = await ti.prompt("What is your name?");
 // remove previous outputs
-ti.clear(); 
+ti.clear();
 
 ti.output("Hello, " + name);
 ti.output("I am Quagmire");
+ti.output("This is my room.");
+let rival = await ti.prompt("What is your rival's name?");
+ti.output("Oh it's " + rival + "!");
+if (await ti.promptYesOrNo("Do you want to see Quahog?")) {introRepeating = false}
 
-
+ti.output("No? Go away until you say yes.")
+}
